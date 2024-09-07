628c2f9ab358d8f0f6f81d6313336167df062176
defjm <git.mojan@gmail.com>
2024-09-07 11:30:26 +0200

    feat():WiP deps table done

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
 # openOwl - Open Source Monitoring and Security
 
 ## Summary
-TBD
+OpenOwl provides analysis and monitoring for dependencies and related security-relevant information. 
+- [ ] List dependencies from local Python projects, or open-souerce GitHub repositories
+- [ ] Check dependencies for existing security advisories, using (which uses https://osv.dev)
 
 ![Streamlit App](assets/streamlit-screenshot.png)
 
 
 ## Getting started
-TBD
+
+Start the fast-api server
+```Bash
+poetry run uvicorn openowl.api:app --reload
+```
+
+Open the Streamlit dashboard
+```Bash
+poetry run streamlit run openowl/app.py 
+```
+
+
 
 ## Further notes
 See [work-in-progress](work-in-progress-notes.md) for upcoming tasks, notes and research
--- openowl/app.py
+++ openowl/app.py
@@ -1,23 +1,37 @@
-import requests
-import streamlit as st
 from pprint import pprint
 
+import streamlit as st
+
 from openowl.clients import OpenOwlClient
-from openowl.logger_config import setup_logger
 from openowl.depsdev_utils import get_deps_table
+from openowl.logger_config import setup_logger
+from openowl.utils import extract_github_library_name, is_github_repo
+
+logger = setup_logger(__name__)
 
 oowl_client = OpenOwlClient()
 
 st.title("Dependency Scan")
-package_name = st.text_input("Package URL", "https://github.com/pandas-dev/pandas")
-
+package_url = st.text_input("Package URL", "https://github.com/pandas-dev/pandas")
 
 # Scan with package version
-res = oowl_client.scan_from_url(
-    "https://github.com/pandas-dev/pandas",
-    package_version="2.2.2"
-)
+res = oowl_client.scan_from_url(package_url)
+
+st.markdown(f"""
+            Current default version of :green-background[{res["package_name"]}] is: :green-background[{res["package_version"]}].
+            """)
 df_deps = get_deps_table(res["package_dependencies"])
-st.dataframe(df_deps, use_container_width=True)
+df_deps = df_deps[1:].sort_values("relation")
 
+num_deps_total = len(df_deps)
+num_deps_direct = len(df_deps.loc[df_deps["relation"]=="DIRECT"])
+num_deps_indirect = len(df_deps.loc[df_deps["relation"]=="INDIRECT"])
 
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("total number of dependencies", num_deps_total)    
+with col2:
+    st.metric("direct dependencies", num_deps_direct)
+with col3:
+    st.metric("indirect dependencies", num_deps_indirect)
+st.dataframe(df_deps[1:], use_container_width=True)
